83be9d56ff64edf74718f19abbb2d13a61c17b8c
José Antonio Martín Baena <jose.antonio.martin.baena@gmail.com>
2013-07-31 00:34:33 +0100

    Trying GradientBoosterClassifier.
    The code of ensemble.py and main.py has been mostly refactored. However,
    I haven't made it generic to account for other models.

--- ensemble.py
+++ ensemble.py
@@ -1,43 +1,50 @@
+import logging
 import numpy as np
-from numpy import array
 import util
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.ensemble import RandomForestRegressor
-from itertools import combinations
+from sklearn import ensemble
+import itertools
 
-def pre_process_data(X,Y):
-	#x1 = array((X.ix[:]))
-	x1 = array(X)
-	x2 = group_data(x1, degree=2)
-	x3 = group_data(x1, degree=3)
-	x_all = np.hstack((x1,x2,x3))
-	
-	#y1 = array((y.ix[:]))
-	y1 = array(Y)
-	y2 = group_data(y1, degree=2)
-	y3 = group_data(y1, degree=3)
-	y_all = np.hstack((y1,y2,y3))
-	'''util.print_debug_msg('Pre processing Data')
-	enc = OneHotEncoder()
-	enc.fit(np.vstack((X,Y)))
-	X = enc.transform(X)
-	Y = enc.transform(Y)'''
-	return x_all, y_all
 
-def group_data(data, degree=3, hash=hash):
-	util.print_debug_msg('Grouping Data')
-	new_data = []
-	m,n = data.shape
-	print m,n
-	for indicies in combinations(range(n), degree):
-		new_data.append([hash(tuple(v)) for v in data[:,indicies]])
-	return array(new_data).T
+log = logging.getLogger(__name__)
+
+
+def pre_process_data(X, Y=None):
+    #x1 = np.array((X.ix[:]))
+    x1 = np.array(X)
+    x2 = group_data(x1, degree=2)
+    x3 = group_data(x1, degree=3)
+    assert x1.shape[0] == x2.shape[0], (
+        'x1.shape ({!r}) != x2.shape ({!r})'.format(x1.shape, x2.shape))
+    assert x2.shape[0] == x3.shape[0], (
+        'x2.shape ({!r}) != x3.shape ({!r})'.format(x2.shape, x3.shape))
+    x_all = np.hstack((x1, x2, x3))
+    
+    if (Y):
+      #y1 = np.array((y.ix[:]))
+      y1 = np.array(Y)
+      y2 = group_data(y1, degree=2)
+      y3 = group_data(y1, degree=3)
+      y_all = np.hstack((y1, y2, y3))
+      return x_all, y_all
+    else:
+      return x_all
+
+
+def group_data(data, degree=3, hash_function=hash):
+    log.info('Grouping data (degree={})...'.format(degree))
+    new_data = []
+    m, n = data.shape
+    log.debug('Shape = {}x{}.'.format(m, n))
+    new_data = [[hash_function(tuple(v))
+                 for v in data[:,indices]]
+                for indices in itertools.combinations(range(n), degree)]
+    return np.array(new_data).T
+
 
 def train_predict(X_train, y_train, X_test):
-	util.print_debug_msg('Training RandomForestRegressor Classifier')
-	model = RandomForestRegressor(compute_importances = True)
-	model.fit(X_train, y_train)
-	print sorted(zip(list(range(93)),model.feature_importances_),  key=lambda tup: tup[1], reverse=True)
-	util.print_debug_msg('Predicting RandomForestRegressor Classifier')
-	return model.predict(X_test)
-
+    model = ensemble.GradientBoostingClassifier()
+    log.info('Training a {}.'
+             .format(model.__class__.__name__))
+    model.fit(X_train, y_train)
+    log.info('Predicting...')
+    return model.predict(X_test)
--- main.py
+++ main.py
@@ -1,61 +1,62 @@
+import logging
 import util
-import logistic_regression as model
-#import ensemble as model
+import ensemble as model
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.cross_validation import StratifiedKFold
 
-train = util.get_train_set()
-#test = util.get_test_set()
+log = logging.getLogger(__name__)
 
-print 'Data Loaded'
 
-def separate_label_and_data():
-	util.print_debug_msg('separating label and data')
-	y = list(train['ACTION'])
-	# creating a temp object of train, so as to preserve its originality.
-	# i an not sure if we really need to do this.
-	X = train
-	del X['ACTION']
-	return X,y
+def load_data():
+    result = util.get_train_set()
+    log.info('Data loaded.')
+    return result
 
+
+def separate_label_and_data(train):
+    log.debug('Separating label and data...')
+    y = list(train['ACTION'])
+    del train['ACTION']
+    return train, y
 
 
 def validate_model():
-	X,y = separate_label_and_data()
-	X = model.pre_process_data(X)
-	#print X.shape
-	#model.select_features(X,y)
-	result_sum = 0
-	K = 10
-	fold = 0
-	skf = StratifiedKFold(y, n_folds = K)
-	for train_index, test_index in skf:
-		fold += 1
-		X_train = []
-		X_test = []
-		Y_train = []
-		Y_test = []
-		for i in train_index:
-			#X_train.append(list(X.ix[i]))
-			X_train.append(X[i])
-			Y_train.append(y[i])
-		for i in test_index:
-			#X_test.append(list(X.ix[i]))
-			X_test.append(X[i])
-			Y_test.append(y[i])
-		
-		print '========== Fold %d ==========' % (fold)
-		X_train = model.normalize_features(X_train).toarray()
-		X_test = model.normalize_features(X_test).toarray()
-		#X_train, X_test = model.pre_process_data(X_train, X_test)
-		pred = model.train_predict(X_train, Y_train, X_test)
-		result = util.calculate_auc(Y_test, pred)
-		result_sum += result
-		print "AUC for fold %d = %0.11f" % (fold,result) 
-	print "Average AUC for this classifier = %0.11f" % (result_sum/K)
+    train = load_data()
+    X, y = separate_label_and_data(train)
+    X = model.pre_process_data(X)
+    #print X.shape
+    #model.select_features(X,y)
+    result_sum = 0
+    K = 10
+    fold = 0
+    skf = StratifiedKFold(y, n_folds = K)
+    for train_index, test_index in skf:
+        fold += 1
+        X_train = []
+        X_test = []
+        Y_train = []
+        Y_test = []
+        for i in train_index:
+            #X_train.append(list(X.ix[i]))
+            X_train.append(X[i])
+            Y_train.append(y[i])
+        for i in test_index:
+            #X_test.append(list(X.ix[i]))
+            X_test.append(X[i])
+            Y_test.append(y[i])
+        
+        log.info('========== Fold {} =========='.format(fold))
+        pred = model.train_predict(X_train, Y_train, X_test)
+        result = util.calculate_auc(Y_test, pred)
+        result_sum += result
+        log.info('AUC for fold {} = {:0.11f}'.format(fold,result))
+    log.info('Average AUC for this classifier = {:0.11f}'.format(result_sum/K))
+
 
 def main():
-	validate_model()
+    logging.basicConfig(level=logging.DEBUG)
+    validate_model()
+
 
 if __name__ == '__main__':
-	main()
+    main()
